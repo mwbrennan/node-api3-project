d49900e27c542e253060248f18e4c77ce7daeaed
mwbrennan <mwb5@me.com>
2021-04-07 14:16:40 -0500

    init

--- api/server.js
+++ api/server.js
@@ -1,8 +1,13 @@
 const express = require('express');
-
+const morgan = require('morgan');
+const helmet = require('helmet');
+const userRoute = require('./users/users-router.js');
 const server = express();
 
 // remember express by default cannot parse JSON in request bodies
+server.use(express.json());
+server.use(helmet());
+server.use(morgan('dev'));
 
 // global middlewares and the user's router need to be connected here
 

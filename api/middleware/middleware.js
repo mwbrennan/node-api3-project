4bb50b31c71a26ff8d0d012311e79439dba28d06
mwbrennan <mwb5@me.com>
2021-04-07 16:00:46 -0500

    mvp

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,62 @@
+const Users = require('../users/users-model.js');
+const Posts = require('../posts/posts-model.js');
+
 function logger(req, res, next) {
   // DO YOUR MAGIC
+  console.log(`timestamp: ${Date.now()} / method: ${req.method} / url: ${req.url}}`)
 }
 
 function validateUserId(req, res, next) {
   // DO YOUR MAGIC
+  const id = req.params.id;
+  try {
+    const user = await Users.getById(id);
+    if(!user){
+      res.status(404).json({ message: 'user not found' });
+    } else {
+      req.user = user
+      next();
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'having some issues' })
+  }
 }
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
+  const { name } = req.body;
+  try {
+    if(!name) {
+      res.status(400).json({ message: 'missing user data' })
+    } else {
+      next()
+    }
+  } catch(error) {
+    res.status(400).json({ message: 'missing required text field' })
+  }
 }
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
+  const id = req.params.id;
+  try {
+    const post = Posts.getById(id);
+    if(!post) {
+      res.status(400).json({ message: 'missing required text field' })
+    } else {
+      req.post = post
+      next();
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'server issues' })
+  }
 }
 
 // do not forget to expose these functions to other modules
+
+module.exports = {
+  logger,
+  validateUser,
+  validateUserId,
+  validatePost
+}
--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const userRoute = require('./users/users-router.js');
+
 const server = express();
 
 // remember express by default cannot parse JSON in request bodies
@@ -10,6 +11,7 @@ server.use(helmet());
 server.use(morgan('dev'));
 
 // global middlewares and the user's router need to be connected here
+server.use('/api/users', userRoute)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
